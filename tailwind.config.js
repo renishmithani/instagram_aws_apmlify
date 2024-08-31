5eb7406d8ccfabb845f91242e5ca0737de41345d
Renish Mithani <renishmithani786110@gmail.com>
2024-08-31 20:48:02 +0530

    Added event emmiter

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,6 @@ module.exports = {
     "./components/**/*.{js,jsx,ts,tsx}", // Include all JS, JSX, TS, and TSX files in the components folder]
     "./app/(tabs)/meditate.tsx",
     "./screens/**/*.{js,jsx,ts,tsx}",
-    "./src/**/*.{html,js}",
   ],
   theme: {
     extend: {
